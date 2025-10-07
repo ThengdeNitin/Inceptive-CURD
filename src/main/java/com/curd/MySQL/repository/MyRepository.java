edb32c4d400c6632e616b1a7b48e84cab2e79f5f
ThengdeNitin <nitinthengde14368@gmail.com>
2025-10-07 16:25:19 +0530

    bug

--- src/main/java/com/curd/MySQL/repository/MyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.curd.MySQL.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.curd.MySQL.entity.User;
-
-public interface MyRepository extends JpaRepository<User, Long> {
-
-}
